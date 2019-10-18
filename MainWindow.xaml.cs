67e5d929fc410106913af74d07a5d37a405cf7d5
brisingr1 <htherrell@harding.edu>
2019-10-18 15:49:32 -0500

    All songs are shown
    All songs are shown.
    Binding attributes were capitalized and was using wrong function to get all songs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,54 +21,23 @@ namespace hTunes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MusicLib musicLib;
         public MainWindow()
         {
             InitializeComponent();
-            List<Song> allSongs = new List<Song>();
+            musicLib = new MusicLib();
+            musicLib.PrintAllTables();
 
-            /*
-            // Loop through all the songs and get info to put into allSongs List
-            allSongs.Add(new Song
-            {
-                Id = 1,
-                Title = "Something",
-                Album = "an album",
-                Artist = "Toby Mac",
-                Genre = "Rock",
-                AlbumImage = "http://www.harding.edu/fmccown/images/broncos.gif"
-            });
-            dataGrid.ItemsSource = allSongs;
-            */
 
             // TODO: Get songs from music.xml (?)
             // TODO: Put those songs in table   
 
-            /*
-            DataTable table = new DataTable("Song");
-            table.Columns.Add(new DataColumn("Id", typeof(int)));
-            table.Columns.Add(new DataColumn("Title", typeof(string)));
-            table.Columns.Add(new DataColumn("Artist", typeof(string)));
-            table.Columns.Add(new DataColumn("AlbumImage", typeof(string)));
 
-            DataRow row = table.NewRow();
-            row["Id"] = 1;
-            row["Title"] = "Good Vibrations";
-            row["Artist"] = "The Beach Boys";
-            row["AlbumImage"] = "http://www.harding.edu/fmccown/images/broncos.gif";
-            table.Rows.Add(row);
+            //DataTable table = musicLib.SongsForPlaylist("Cool stuff!");
+            DataTable table = musicLib.Songs;
 
-            row = table.NewRow();
-            row["Id"] = 2;
-            row["Title"] = "Love Me Tender";
-            row["Artist"] = "Elvis Presley";
-            row["AlbumImage"] = "http://www.harding.edu/fmccown/images/cowboys.png";
-            table.Rows.Add(row);
-            */
-
-
-
-            MusicLib musicLib = new MusicLib();
-            DataTable table = musicLib.SongsForPlaylist("Cool Stuff!");
+            // Found string[] for songs
+            // string allSongs[] = musicLib.SongIds;  
 
             // Bind the data source
             dataGrid.ItemsSource = table.DefaultView;
